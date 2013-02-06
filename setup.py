67d2f3b9d50291cd9dafc2b35f3c86b80105b9bf
Bouke Haarsma <bouke@webatoom.nl>
2013-02-06 12:32:18 +0100

    Experimental Python 3.3 support

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup
-
+import sys
 import airbrake
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
 
 setup(
     name='django-airbrake',
@@ -26,7 +30,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 2 :: Only',
         'Topic :: System :: Logging',
-    ]
+    ],
+    **extra
 )
