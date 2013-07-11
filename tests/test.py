e3d2959bcc8775a067861deb4f96f1079f4bf68b
Bouke Haarsma <bouke@webatoom.nl>
2013-07-11 17:07:01 +0200

    Validate xml against xsd, fixes #3

--- tests/test.py
+++ tests/test.py
@@ -1,13 +1,16 @@
 import logging
 import unittest
-from xml.etree.ElementTree import fromstring
+
+from lxml import etree
 from django import conf
 try:
     from django.conf.urls import url, patterns
 except ImportError:  # django 1.3
     from django.conf.urls.defaults import url, patterns
 from mock import Mock
+
 from airbrake.handlers import AirbrakeHandler
+from utils import xml_compare, xsd_validate
 
 
 class Test(unittest.TestCase):
@@ -27,7 +30,9 @@ class Test(unittest.TestCase):
             self.logger.exception(exc)
         xml = self.mock.call_args[0][0]
 
-        self.assertTrue(xml_compare(fromstring("""
+        xsd_validate(etree.fromstring(xml))
+
+        self.assertTrue(xml_compare(etree.fromstring("""
         <notice version="2.0">
             <api-key>MY_API_KEY</api-key>
             <notifier>
@@ -48,7 +53,7 @@ class Test(unittest.TestCase):
                 </backtrace>
             </error>
         </notice>
-        """), fromstring(xml), self.fail))
+        """), etree.fromstring(xml), self.fail))
 
     def test_django_request(self):
         def myview(request):
@@ -70,7 +75,10 @@ class Test(unittest.TestCase):
             self.logger.error(str(exc), exc_info=1, extra={'request': request})
 
         xml = self.mock.call_args[0][0]
-        self.assertTrue(xml_compare(fromstring("""
+
+        xsd_validate(etree.fromstring(xml))
+
+        self.assertTrue(xml_compare(etree.fromstring("""
         <notice version="2.0">
             <api-key>MY_API_KEY</api-key>
             <notifier>
@@ -106,62 +114,5 @@ class Test(unittest.TestCase):
                 </backtrace>
             </error>
         </notice>
-        """ % {'name': __name__}), fromstring(xml), self.fail))
+        """ % {'name': __name__}), etree.fromstring(xml), self.fail))
 
-
-def xml_compare(x1, x2, reporter=None):
-    if x1.tag != x2.tag:
-        if reporter:
-            reporter('Tags do not match: %s and %s' % (x1.tag, x2.tag))
-        return False
-    for name, value in x1.attrib.items():
-        if not text_compare(x2.attrib.get(name), value):
-            if reporter:
-                reporter('<%s> Attributes do not match: %s=%r, %s=%r'
-                         % (x1.tag, name, value, name, x2.attrib.get(name)))
-            return False
-    for name in x2.attrib.keys():
-        if name not in x1.attrib:
-            if reporter:
-                reporter('<%s> x2 has an attribute x1 is missing: %s'
-                         % (x1.tag, name))
-            return False
-    if not text_compare(x1.text, x2.text):
-        if reporter:
-            reporter('<%s> text: %r != %r' % (x1.tag, x1.text, x2.text))
-        return False
-    if not text_compare(x1.tail, x2.tail):
-        if reporter:
-            reporter('<%s> tail: %r != %r' % (x1.tag, x1.tail, x2.tail))
-        return False
-    cl1 = x1.getchildren()
-    cl2 = x2.getchildren()
-    if len(cl1) != len(cl2):
-        if reporter:
-            reporter('<%s> children length differs, %i != %i'
-                     % (x1.tag, len(cl1), len(cl2)))
-        return False
-    for i, c1 in enumerate(cl1):
-        for c2 in cl2:
-            if xml_compare(c1, c2):  # no reporter, fail silently
-                cl2.remove(c2)
-                break
-        else:
-            if reporter:
-                xml_compare(c1, x2.getchildren()[i], reporter=reporter)
-                reporter('<%s> children %i do not match: %s'
-                         % (x1.tag, i, c1.tag))
-            return False
-    return True
-
-
-def text_compare(t1, t2):
-    if not t1 and not t2:
-        return True
-    if t1 == '*' or t2 == '*':
-        return True
-    return (t1 or '').strip() == (t2 or '').strip()
-
-
-if __name__ == '__main__':
-    unittest.main()
---
+++ tests/utils.py
@@ -0,0 +1,61 @@
+from lxml import etree
+
+
+def xml_compare(x1, x2, reporter=None):
+    if x1.tag != x2.tag:
+        if reporter:
+            reporter('Tags do not match: %s and %s' % (x1.tag, x2.tag))
+        return False
+    for name, value in x1.attrib.items():
+        if not text_compare(x2.attrib.get(name), value):
+            if reporter:
+                reporter('<%s> Attributes do not match: %s=%r, %s=%r'
+                         % (x1.tag, name, value, name, x2.attrib.get(name)))
+            return False
+    for name in x2.attrib.keys():
+        if name not in x1.attrib:
+            if reporter:
+                reporter('<%s> x2 has an attribute x1 is missing: %s'
+                         % (x1.tag, name))
+            return False
+    if not text_compare(x1.text, x2.text):
+        if reporter:
+            reporter('<%s> text: %r != %r' % (x1.tag, x1.text, x2.text))
+        return False
+    if not text_compare(x1.tail, x2.tail):
+        if reporter:
+            reporter('<%s> tail: %r != %r' % (x1.tag, x1.tail, x2.tail))
+        return False
+    cl1 = x1.getchildren()
+    cl2 = x2.getchildren()
+    if len(cl1) != len(cl2):
+        if reporter:
+            reporter('<%s> children length differs, %i != %i'
+                     % (x1.tag, len(cl1), len(cl2)))
+        return False
+    for i, c1 in enumerate(cl1):
+        for c2 in cl2:
+            if xml_compare(c1, c2):  # no reporter, fail silently
+                cl2.remove(c2)
+                break
+        else:
+            if reporter:
+                xml_compare(c1, x2.getchildren()[i], reporter=reporter)
+                reporter('<%s> children %i do not match: %s'
+                         % (x1.tag, i, c1.tag))
+            return False
+    return True
+
+
+def text_compare(t1, t2):
+    if not t1 and not t2:
+        return True
+    if t1 == '*' or t2 == '*':
+        return True
+    return (t1 or '').strip() == (t2 or '').strip()
+
+
+def xsd_validate(tree):
+    xml_doc = etree.parse('schema.xsd')
+    xml_schema = etree.XMLSchema(xml_doc)
+    assert xml_schema.validate(tree)
